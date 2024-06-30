b3f86342d6f2d2b25b881a83980f09756d04534a
Tekalig <tekahazi06@gmail.com>
2024-06-30 14:27:44 +0300

    add package.json file

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const cors = require("cors");
 
 const {passengerRouter}  = require("./routes/passenger/passenger.router")
-
+// init express app
 const app = express();
-app.use(cors());
+// allowing cross origin resource service
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+  })
+);
 app.use(express.json());
 
 // route to passenger 
