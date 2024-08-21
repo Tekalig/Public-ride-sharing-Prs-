96bf9c9ac7f791e72ff1e8e6b534cb4629a886ce
Tekalig <tekahazi06@gmail.com>
2024-08-21 21:05:52 +0300

    add context provider to the app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ColorModeScript, ChakraProvider, theme } from "@chakra-ui/react";
-import "./styles/general.css";
+import "./index.css";
 
 import App from "./App.jsx";
+import ContextProvider from "./context/ContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
