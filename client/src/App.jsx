ea5bc0b5d684582b60274cebb00a98228c99612c
Tekalig <tekahazi06@gmail.com>
2024-08-21 20:40:00 +0300

    create new route and changing import path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/HomePage/components/Home";
-import Account from "./pages/Account Page/components/Account";
-import Driver from "./pages/Driver App Page/components/Driver";
-import RideRequest from "./pages/Ride Request Page/components/RideRequest";
-import About from "./pages/About us Page/components/About";
+import Home from "./pages/HomePage/Home";
+import Account from "./pages/Account Page/Account";
+import Driver from "./pages/Driver App Page/Driver";
+import RideRequest from "./pages/Ride Request Page/RideRequest";
+import About from "./pages/About us Page/About";
 import DriverLogin from "./pages/Driver/Auth/Login";
 import DriverRegister from "./pages/Driver/Auth/SignUp";
 import PassangerRegister from "./pages/Passenger/Auth/SignUp";
 import PassangerLogin from "./pages/Passenger/Auth/Login";
 import "./App.css";
+import Setting from "./pages/Account Page/Settings component/Setting";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/driver" element={<Driver />} />
           <Route path="/ride-request" element={<RideRequest />} />
           <Route path="/about" element={<About />} />
+          <Route path="/edit" element={<Setting />} />
           <Route path="/driver-login" element={<DriverLogin />} />
           <Route path="/driver-register" element={<DriverRegister />} />
           <Route path="/passanger-register" element={<PassangerRegister />} />
