352da3c837dc07887a43140fdcdbe8f258b8d32b
Tekalign Mesfin <tekahazi06@gmail.com>
2024-12-22 23:20:27 +0300

    passenger init start
    - passenger login
    - passenger signup
    - passenger model

--- server/src/models/passenger_model.js
+++ server/src/models/passenger_model.js
@@ -1,32 +1,43 @@
-const bcrypt = require("bcrypt");
-import db from "../server";
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db'); // Adjust the path as necessary
 
-async function newPassenger([user_name, email, password, phoneNumber]) {
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const sql =
-    "INSERT INTO users (user_name, email, password, phone_Number) VALUES (?, ?, ?, ?)";
-  await db.query(
-    sql,
-    [user_name, email, hashedPassword, phoneNumber],
-    (error, data) => {
-      if (error) {
-        return new Error({ error: "Database error" });
-      } else {
-        return data.insertId;
-      }
-    }
-  );
-}
+const Passenger = sequelize.define(
+  "Passenger",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    tableName: "passengers",
+  }
+);
 
-async function verifyPassenger({ email, password }) {
-  const sql = "SELECT * FROM users WHERE email = ?";
-  await db.query(sql, [email], async (error, data) => {
-    if (error) {
-      return new Error({ error: "Database error" });
-    }
-    if (data.length > 0) {
-      const user = data[0];
-      return await bcrypt.compare(password, user.password);
-    }
-  });
-}
+module.exports = Passenger;
