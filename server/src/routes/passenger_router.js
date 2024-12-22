91a620d8728f15f7c1016897c260fdfef37d0f8f
Tekalign Mesfin <tekahazi06@gmail.com>
2024-12-22 23:20:27 +0300

    driver init start
    - driver login
    - driver signup
    - driver model

--- server/src/routes/passenger_router.js
+++ server/src/routes/passenger_router.js
@@ -1,8 +1,33 @@
-const express = require("express");
-const { passengerLogin, passengerRegister } = require("../controllers/passenger_controller");
+/**
+ * @fileoverview Defines the routes for passenger-related operations.
+ * @module routes/passenger_router
+ */
 
-const passengerRouter = express.Router();
+const express = require("express"); // Import the express module
+const { passengerLogin, passengerSignup } = require("../controllers/passenger_controller"); // Import passenger controller functions
+
+const passengerRouter = express.Router(); // Create a new router instance
+
+/**
+ * Route for passenger login.
+ * @name post/login
+ * @function
+ * @memberof module:routes/passenger_router
+ * @inner
+ * @param {string} path - Express path
+ * @param {function} passengerLogin - Controller function for passenger login
+ */
 passengerRouter.post("/login", passengerLogin);
-passengerRouter.post("/register", passengerRegister);
 
-module.exports = passengerRouter;
+/**
+ * Route for passenger registration.
+ * @name post/signup
+ * @function
+ * @memberof module:routes/passenger_router
+ * @inner
+ * @param {string} path - Express path
+ * @param {function} passengerSignup - Controller function for passenger registration
+ */
+passengerRouter.post("/signup", passengerSignup);
+
+module.exports = passengerRouter; // Export the router instance
